4c7b022c758136d861d25619ce47ede6efbf4fc0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-05-19 10:23:12 +0900

    Animal Inspection
    -오브젝트가 초록색 아웃라인이 뜬다면 조사 가능한 상태.
    
    -E 키를 눌러 객체의 이름을 받아온다
    
    -collider 에서는 mesh 를 가지고 있는 자식 오브젝트가 받아와지기 때문에 따로 부모 오브젝트를 받아오는 작업 필요

--- NCT/Assets/Scripts/Interaction.cs
+++ NCT/Assets/Scripts/Interaction.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
+    bool isIntercation; //조사가 가능한지 나타내는 bool
+    GameObject go;      //조사할 오브젝트를 넣어두는 임시 변수
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && isIntercation)
+        {
+            Interactioning(go);
+        }
+    }
+
+
+    void Interactioning(GameObject ob)
+    {
+        Debug.Log(ob);
+    }
 
     private void OnTriggerStay(Collider collider)
     {
@@ -11,14 +27,19 @@ public class Interaction : MonoBehaviour
         if (Physics.Raycast(transform.position, forward, 10) && collider.CompareTag("Animal")) // 캐릭터가 바라보고 있고, 동물일 경우
         {
             collider.GetComponent<Outline>().enabled = true; // Outline 활성화
-            print("동물이 감지 되었습니다.");
+            isIntercation = true;   //조사가능
+
+            go = collider.transform.parent.gameObject;  //mesh 오브젝트의 부모 오브젝트 받아오기
         }
     }
 
     private void OnTriggerExit(Collider collider)
     {
+        isIntercation = false;  //조사 불가능
+
         if (collider.CompareTag("Animal")) // 동물일 경우
         {
+            go = null;  //객체 초기화
             collider.GetComponent<Outline>().enabled = false; // Outline 비활성화
         }
     }
