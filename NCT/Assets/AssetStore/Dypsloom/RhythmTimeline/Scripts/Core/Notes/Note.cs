f6b09baa131cf216df87657cf353ce59bb4e3e14
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-05-25 16:07:53 +0900

    Note.cs 주석

--- NCT/Assets/AssetStore/Dypsloom/RhythmTimeline/Scripts/Core/Notes/Note.cs
+++ NCT/Assets/AssetStore/Dypsloom/RhythmTimeline/Scripts/Core/Notes/Note.cs
@@ -15,7 +15,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 	using UnityEngine.Events;
 
 	/// <summary>
-	/// The note trigger event data is used to hold information about the trigger event.
+	/// 트리거 이벤트 정보를 저장하는데 사용
 	/// </summary>
 	public class NoteTriggerEventData
 	{
@@ -48,18 +48,18 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 	}
 
 	/// <summary>
-	/// The states for the note.
+	/// 노트를 위한 상태
 	/// </summary>
 	public enum ActiveState
 	{
-		Disabled,	// When the Note has not been Initialized yet
-		PreActive,	// Between the note being intialized and the active state
-		Active,		// While the note is active
-		PostActive	// While the note has been deactivated but not reinitialized.
+		Disabled,	// 노트가 아직 초기화 되지않은 경우
+		PreActive,	// 초기화 중인 노트와 활성 상태 사이
+		Active,		// 노트가 활성 상태
+		PostActive	// 노트가 비활성화되어 있지만 다시 초기화 되어있지 않은 상태
 	}
 
 	/// <summary>
-	/// The base class for the note component.
+	/// 노트 구성 요소의 기본 클래스
 	/// </summary>
 	public abstract class Note : MonoBehaviour
 	{
@@ -69,13 +69,13 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		public event Action<Note> OnActivate;
 		public event Action<Note> OnDeactivate;
 
-		[Tooltip("If set to false the Note will be updated using the rhythm director start DSP time as single truth, instead of the timeline current time.")]
-		[SerializeField] protected bool m_UpdateWithTimeline = true;
-		[Tooltip("If set to false the Note will activate with time which will give more precision.")]
+		[Tooltip("false로 설정하면 타임라인의 현재 시간 대신 리듬 디랙터 시작 DSP(디지털 신호 처리) 시간을 단일 트루로 사용하여 노트가 업데이트 됩니다.")]
+		[SerializeField] protected bool m_UpdateWithTimeline = true;    //true로 해둘 것
+		[Tooltip("false로 설정하면 노트가 시간과 함께 활성화되어 더 정확한 정보를 제공합니다.")]
 		[SerializeField] protected bool m_ActivateWithClip = false;
-		[Tooltip("Orient the rotation of the Note to match the track rotation?")]
+		[Tooltip("노트의 회전 방향을 트랙 회전과 일치하도록 조정하겠습니까?")]
 		[SerializeField] protected bool m_OrientToTrack = true;
-		[Tooltip("The game object to set active while the clip is running.")]
+		[Tooltip("클립이 실행될동안 활성화될 게임 개체")]
 		[SerializeField] protected GameObject m_SetActiveWhileClipActive;
 	
 		protected RhythmClipData m_RhythmClipData;
@@ -118,9 +118,9 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		{ }
 
 		/// <summary>
-		/// Initialize the note using the rhythm clip data.
+		/// 리듬클립데이터를 사용하여 노트를 초기화 합니다
 		/// </summary>
-		/// <param name="rhythmClipData">The rhythm clip data.</param>
+		/// <param name="rhythmClipData">리듬클립데이터</param>
 		public virtual void Initialize(RhythmClipData rhythmClipData)
 		{
 			if (rhythmClipData.TrackObject == null) {
@@ -154,7 +154,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 
 		/// <summary>
-		/// Reset when the note is returned to the pool.
+		/// 노트를 pool로 반환할때 재설정
 		/// </summary>
 		public virtual void Reset()
 		{
@@ -191,8 +191,8 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 
 		/// <summary>
-		/// The note needs to be activated as it is within range of being triggered.
-		/// This usually happens when the clip starts.
+		/// 노트는 트리거 될 수 있는 범위 내에 있으므로 활성화해야 합니다.
+		/// 보통 클립이 시작될 때 발생합니다. 
 		/// </summary>
 		protected virtual void ActivateNote()
 		{
@@ -214,7 +214,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 
 		/// <summary>
-		/// The note was deactivated.
+		/// 노트가 비활성화 되었다.
 		/// </summary>
 		protected virtual void DeactivateNote()
 		{
@@ -271,7 +271,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 
 		/// <summary>
-		/// The timeline update, updates every frame and in edit mode too.
+		/// 타임라인을 업데이트, 모든 프레임 및 편집 모드도 업데이트
 		/// </summary>
 		/// <param name="globalClipStartTime">The offset to the clip start time.</param>
 		/// <param name="globalClipEndTime">The offset to the clip stop time</param>
@@ -291,7 +291,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 	
 		/// <summary>
-		/// Default update can be used instead of timeline update to sync with DSP adaptive time.
+		/// 시간 표시줄 업데이트 대신 기본 업데이트를 사용하여 DSP 적응 시간과 동기화 할 수 있습니다.
 		/// </summary>
 		protected virtual void Update()
 		{
@@ -310,7 +310,7 @@ namespace Dypsloom.RhythmTimeline.Core.Notes
 		}
 
 		/// <summary>
-		/// Hybrid update works both in play and edit mode.
+		/// 하이브리드 업데이트는 재생모드와 편집 모드에서 모두 작동합니다.
 		/// </summary>
 		/// <param name="timeFromStart">The offset before the start.</param>
 		/// <param name="timeFromEnd">The offset before the end.</param>
