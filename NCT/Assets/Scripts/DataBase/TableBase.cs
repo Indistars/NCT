be8b36b11116063e0ea983321425cc6881d5e142
박재만 <82390527+qkrwoaks@users.noreply.github.com>
2022-07-06 23:37:27 +0900

    Revert "Revert "Feature inspection""

---
+++ NCT/Assets/Scripts/DataBase/TableBase.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+public class TableBase
+{
+    public virtual void SetJsonData(string key, JObject info)
+    {
+
+    }
+}
---
+++ NCT/Assets/Scripts/FeatureTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 데모씬 스크립트
+/// </summary>
+public class FeatureTest : MonoBehaviour
+{
+    string log;
+
+
+    [Header("Test Button List")]
+    public List<TestButton> Buttons;
+
+
+    [Serializable]
+    public class TestButton
+    {
+        public string text;
+        public UnityEvent onClick;
+    }
+
+
+    void OnGUI()
+    {
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one * 3);
+
+
+        foreach (var button in Buttons)
+        {
+            if (GUILayout.Button(button.text))
+            {
+                button.onClick?.Invoke();
+            }
+        }
+
+
+        GUILayout.Label(log);
+    }
+}
