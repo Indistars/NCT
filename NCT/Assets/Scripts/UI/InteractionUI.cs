1e8051cb7645a7af0a8f138b4c8a09dedc68d2bb
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-07-27 19:35:41 +0900

    Rhythm Scene Name 변경

--- NCT/Assets/Scripts/UI/InteractionUI.cs
+++ NCT/Assets/Scripts/UI/InteractionUI.cs
@@ -98,7 +98,7 @@ public class InteractionUI : MonoBehaviour
     public void StartCook()
     {
         Init();
-        SceneManager.LoadScene("RhythmTestScene"); // ¸®µë°ÔÀÓ ¾À ·Îµù
+        SceneManager.LoadScene("RhythmDemoScene"); // ¸®µë°ÔÀÓ ¾À ·Îµù
     }
 
     /// <summary>
