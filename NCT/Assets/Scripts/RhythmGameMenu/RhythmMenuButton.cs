f7dfeded6671411a39c48527648fab040bdac3e6
Lee Joon <jun29334@naver.com>
2022-07-28 09:19:25 +0900

    [ADD] GameMenu
    Scroll View
    UserData Script

---
+++ NCT/Assets/Scripts/RhythmGameMenu/RhythmMenuButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 각 메뉴 버튼마다 저장하고 있을 정보들
+/// </summary>
+public class RhythmMenuButton : MonoBehaviour
+{
+    public string RhythmTitle; /* 노래 이름 */
+    public string RhythmDescription; /* 노래 설명 */
+}
---
+++ NCT/Assets/Scripts/User/UserData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public class UserData
+{
+    public string name;
+    public Dictionary<int, string> rhythmList = new Dictionary<int, string>();
+}
