57b2a676cdffac20aae9840c223e9db7a73df5ed
박재만 <s21009@gsm.hs.kr>
2022-09-14 16:30:08 +0900

    동물 불러오기 코드 수정

--- NCT/Assets/Scripts/AnimalImport.cs
+++ NCT/Assets/Scripts/AnimalImport.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Import : MonoBehaviour
+public class AnimalImport : Singleton<AnimalImport>
 {
+    public int count;
+
     [SerializeField] private GameObject[] animalModels; // 동물 모델을 가져오는 변수
     [SerializeField] private Transform animalSpawnPoint; // 소환할 동물 위치를 가지고 있는 변수
 
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(gameObject); // 삭제 되지 않게 설정
         DataBase.DataBaseManager.Instance.LoadAnimalTable(); /* tdAnimal 불러오기, 파싱 ※나중에 게임 로딩창에서 사용 예정※ */
-        SpawnAnimal(0); // 동물 소환 함수
+        SpawnAnimal(count); // 동물 소환 함수
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-
-    //}
+    public void SpawnAnimal()
+    {
+        Instantiate(animalModels[count], animalSpawnPoint); // 해당하는 인덱스의 동물을 소환
+        Debug.Log("동물 :" + animalModels[count].name.ToString() + "이 스폰 되었습니다!"); // 알림
+        count++;
+    }
 
     /// <summary>
     /// 동물을 소환하는 함수
